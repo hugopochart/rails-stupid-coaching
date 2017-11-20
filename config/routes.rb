35764cf8b69a266237fe40c79fc58021edf98908
Hugo Pochart <hugo.pochart@gmail.com>
2017-11-20 13:00:34 +0100

    stupid coaching

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  root to: 'questions_controller#home'
+
+  get 'questions_controller/answer'
+
+  get 'questions_controller/ask'
+
+  get 'pages/coach'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
